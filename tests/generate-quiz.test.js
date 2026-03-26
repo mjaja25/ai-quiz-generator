f8398570e235db477b7044e72a30416d39873ce7
mjaja25 <mhajan346@gmail.com>
2026-03-26 16:52:50 +0530

    Move test file out of netlify/functions to fix Netlify deploy

--- tests/generate-quiz.test.js
+++ tests/generate-quiz.test.js
@@ -1,7 +1,7 @@
 const { describe, it } = require("node:test");
 const assert = require("node:assert/strict");
 
-const { handler } = require("./generate-quiz.js");
+const { handler } = require("../netlify/functions/generate-quiz.js");
 
 function mockEvent(method = "POST", body = null, origin = "") {
   return {
